6684ee6f0c4705360936328546d32634b801dee4
marve10s <igrimanigroman@gmail.com>
2024-04-10 22:04:10 +0300

    small fixes

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = 'cd762bcd-b590-4475-b483-89aad2735524';
+export const KEY = '11c9173a-c64b-4e32-9969-ad9556c86960';
 export const MODAL_CLOSE_SEC = 2.5;
